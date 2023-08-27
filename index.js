cdfd209a3ce76bc999c90d13f74f5b54ef3c4cbf
maluyacode <adlawandavemerc98@gmail.com>
2023-08-27 13:00:39 +0800

    commented-horizontal-navbar

--- index.js
+++ index.js
@@ -1,4 +1,50 @@
-let form = document.querySelector('#message-me');
+// not finished
+// let form = document.querySelector('#message-me');
+// let navBar = document.querySelector('#navbar');
+// let header = document.querySelector('#header');
+// let burger = document.querySelector('.burger');
+// let state = "hide"
+
+
+// window.addEventListener('resize', navigation);
+// burger.addEventListener('click', showNav);
+
+// function navigation() {
+
+//     let widthScreenSize = window.innerWidth;
+
+
+//     if (widthScreenSize < 800) {
+//         navBar.style.display = "none"
+//         burger.style.display = "block"
+
+//     } else {
+//         navBar.style.display = "flex"
+//         burger.style.display = "none"
+//     }
+// }
+
+// function showNav() {
+
+
+//     if (state == 'hide') {
+//         state = "show"
+//         burger.setAttribute('data-state', state)
+//         header.setAttribute('id', 'horizontalHeader')
+//         navBar.setAttribute('id', 'horizontalNavbar')
+//         navBar.style.display = "flex"
+//     } else {
+//         state = "hide"
+//         burger.setAttribute('data-state', state)
+//         header.setAttribute('id', 'header')
+//         navBar.setAttribute('id', 'navbar')
+
+//         navBar.style.display = "none"
+//     }
+// }
+
+// navigation()
+// not finished
 
 form.addEventListener('submit', function (e) {
     e.preventDefault();
