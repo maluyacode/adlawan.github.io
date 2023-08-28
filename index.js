a065e0529deef73b081ed8e4b81b18f6d8a1b421
maluyacode <adlawandavemerc98@gmail.com>
2023-08-28 19:16:12 +0800

    burger-horizontal-navbar

--- index.js
+++ index.js
@@ -1,49 +1,66 @@
 // not finished
-// let form = document.querySelector('#message-me');
-// let navBar = document.querySelector('#navbar');
-// let header = document.querySelector('#header');
-// let burger = document.querySelector('.burger');
-// let state = "hide"
+let form = document.querySelector('#message-me');
+let navBar = document.querySelector('#navbar');
+let header = document.querySelector('#header');
+let burger = document.querySelector('.burger');
+let navLinks = document.querySelectorAll('#navbar a')
+// console.log(navLinks);
+let state = "hide"
 
 
-// window.addEventListener('resize', navigation);
-// burger.addEventListener('click', showNav);
+window.addEventListener('resize', navigation);
+burger.addEventListener('click', showNav);
 
-// function navigation() {
+// navLinks.forEach(element => {
 
-//     let widthScreenSize = window.innerWidth;
+//     element.addEventListener('click', showNav)
+//     console.log("Asdas");
 
-
-//     if (widthScreenSize < 800) {
-//         navBar.style.display = "none"
-//         burger.style.display = "block"
-
-//     } else {
-//         navBar.style.display = "flex"
-//         burger.style.display = "none"
-//     }
-// }
-
-// function showNav() {
+// });
 
 
-//     if (state == 'hide') {
-//         state = "show"
-//         burger.setAttribute('data-state', state)
-//         header.setAttribute('id', 'horizontalHeader')
-//         navBar.setAttribute('id', 'horizontalNavbar')
-//         navBar.style.display = "flex"
-//     } else {
-//         state = "hide"
-//         burger.setAttribute('data-state', state)
-//         header.setAttribute('id', 'header')
-//         navBar.setAttribute('id', 'navbar')
+function navigation() {
 
-//         navBar.style.display = "none"
-//     }
-// }
+    let widthScreenSize = window.innerWidth;
 
-// navigation()
+    if (state == 'show' && widthScreenSize < 800) {
+        state = "hide"
+        return;
+    }
+
+    if (widthScreenSize < 800) {
+        navBar.style.display = "none"
+        burger.style.display = "block"
+        header.setAttribute('id', 'horizontalHeader')
+        navBar.setAttribute('id', 'horizontalNavbar')
+
+    } else {
+        navBar.style.display = "flex"
+        burger.style.display = "none"
+        header.setAttribute('id', 'header')
+        navBar.setAttribute('id', 'navbar')
+    }
+}
+
+function showNav() {
+
+    if (state == 'hide') {
+        state = "show"
+        burger.setAttribute('data-state', state)
+        // header.setAttribute('id', 'horizontalHeader')
+        // navBar.setAttribute('id', 'horizontalNavbar')
+        navBar.style.display = "flex"
+    } else {
+        state = "hide"
+        burger.setAttribute('data-state', state)
+        // header.setAttribute('id', 'header')
+        // navBar.setAttribute('id', 'navbar')
+
+        navBar.style.display = "none"
+    }
+}
+
+navigation()
 // not finished
 
 form.addEventListener('submit', function (e) {
